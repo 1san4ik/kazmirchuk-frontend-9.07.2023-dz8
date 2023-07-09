291bc7f898bb5db19832232548d5708b3cf736a8
1san4ik <1san4ik@gmail.com>
2023-07-09 04:00:33 +0300

    Task 2

--- insex.js
+++ insex.js
@@ -6,3 +6,13 @@ for (let a = 10; a <= 20; a++) {
 }
 let i = userMass.join(', ')
 console.log('Task 1: ==> ', i)
+
+// Task 2
+let userMass2 = []
+for (let a2 = 10; a2 <= 20; a2++) {
+    b2 = a2**2
+    userMass2.push(b2)
+    
+}
+let i2 = userMass2.join(', ')
+console.log('Task 2: ==> ', i2)
